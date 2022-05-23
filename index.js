3f4b1d1e4894bb432cb3b5c5cf96b723e318f899
carolxavier <xavier.biotec@gmail.com>
2022-05-23 18:44:09 -0300

    Code without comments and working

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 import express from "express";
-// import multer from "multer";
+import multer from "multer";
 
 const app = express();
 
-// const storage = multer.diskStorage({
-//     destination: (req,file,callback) => {
-//         callback(null, 'uploads/')
-//     },
-//     filename: (req,file,callback) => {
-//         callback(null, file.originalname+'-'+Date.now())
-//     }
-// });
-// const upload = multer({dest:'uploads/'});
+const storage = multer.diskStorage({
+    destination: (req,file,callback) => {
+        callback(null, 'uploads/');
+    },
+    filename: (req,file,callback) => {
+        callback(null, file.originalname+'-'+Date.now());
+    }
+});
+const upload = multer({storage});
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => res.render('home'));
 
-app.post('/', (req,res) => {
+app.post('/', upload.single('test'), (req,res) => {
     console.log(req.body, req.file);
     res.send("ok");
 });
 
 app.listen(3000, () => console.log("App running on port 3000"));
 
-//upload.single('test'), 
